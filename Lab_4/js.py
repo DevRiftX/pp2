1888a81f767384a496de29294cbd01149c02139b
DevRiftX <m_semey@kbtu.kz>
2025-02-19 10:15:13 +0500

    lab_4_update

--- Lab_4/js.py
+++ Lab_4/js.py
@@ -15,4 +15,17 @@ for data in fcc_data["imdata"]:
     temp = []
     for i in range(4):
         temp.append(((data["l1PhysIf"])["attributes"])[values[i]])
-    print(f"{temp[0]}            {temp[1]}                  {temp[2]}   {temp[3]}")
+    print(f"{temp[0]:50} {temp[1]:20}  {temp[2]:6}  {temp[3]:6}")
+
+def fetch(data):
+    for i in data["imdata"]:
+        file = (i["l1PhysIf"])["attributes"]
+        yield f"{file['dn']:50} {file['descr']:20} {file['speed']:6}   {file['mtu']:6}"
+
+print("""Interface Status
+================================================================================
+DN                                                 Description           Speed    MTU  
+-------------------------------------------------- --------------------  ------  ------""")
+
+for x in fetch(fcc_data):
+    print(x)
